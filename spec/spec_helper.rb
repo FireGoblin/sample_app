4c63c9b87fda2f9513eb934b9e0379964be7ffd9
FireGoblin <FireGoblin21@gmail.com>
2011-07-15 21:29:48 -0400

    Done with sign in

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,7 +30,7 @@ RSpec.configure do |config|
   end
 
   def integration_sign_in(user)
-  	visit signing_path
+  	visit signin_path
   	fill_in :email,		:with => user.email
   	fill_in :password, 	:with => user.password
   	click_button
